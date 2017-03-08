7959b6ffb9b1d0312a4cab3eca7b8f8fed31bfe5
spencermcghin <spencer.mcghin@rittmanmead.com>
2017-03-07 20:09:22 -0800

    fixed save functionality

--- myblog/views.py
+++ myblog/views.py
@@ -34,14 +34,14 @@ def detail_view(request, post_id):
     return render(request, 'detail.html', context)
 
 def post_new(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PostForm(request.POST)
-    if form.is_valid():
-        post = form.save(commit=False)
-        post.author = request.user
-        post.published_date = timezone.now()
-        post.save()
-        return redirect('post_detail', pk=post.pk)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
